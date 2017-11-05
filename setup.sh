3dc1439caf09d2da9b84bb43e43894b8704720aa
Dylan Cascio <Dcascio73@gmail.com>
2017-11-05 09:17:25 -0800

    Last commit before pushing to Github

--- setup.sh
+++ setup.sh
@@ -1,30 +1,36 @@
 #!/bin/bash
 
-# make sure we're in right area and create dotfiles/old/
-cd $HOME/dotfiles
-if ! [ -e 'old/' ]; then
-  mkdir old/
-fi
-
 # create list of files to move
 
 FILES=('.zshrc' '.tmux.conf' '.vimrc')
+dots=$HOME/dotfiles
+olddots=$HOME/dotfiles/old
+
+# make sure we're in right area and create dotfiles/old/
+cd dots
+if ! [ -e $olddots/ ]; then
+  echo Making old file directory
+  mkdir $olddots/
+  echo ...done
+fi
 
 # functions
 create_symlink () {
-  ln -s $HOME/dotfiles/$file $HOME/$file
+  ln -s $dots/$file $HOME/$file
 }
-for file in $FILES
-do
+for file in $FILES; do
   if [ -e "$HOME/$file" ] && ! [ -L "$HOME/$file" ] ; then
     echo Moving old ${file}...
     # move old files into d/o/
-    mv $HOME/$file $HOME/dotfiles/old/
+    mv $HOME/$file $olddots/
     # symlink into $HOME
+    echo ...done\nCreating symlink
     create_symlink
+    echo ...done
   elif ! [ -e "$HOME/$file" ] ; then
     echo ~/${file} has not been created,\nCreating symlink.
     create_symlink
+    echo ...done
   else
     echo You\'ve already run this script.
   fi
@@ -39,11 +45,10 @@ else
 fi
 
 # check of oh-my-zsh/
-if ! [ -e "oh-my-zsh/" ]; then
+if [ -e "$HOME/oh-my-zsh" ]; then
+  echo You already have Oh My Zsh installed.
+else
   echo Installing Oh My Zsh...
   sh -c "$(curl -ffSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-  continue
-  echo Oh My Zsh installed.
-else
-  echo You already have Oh My Zsh installed.
+  echo ...done
 fi
